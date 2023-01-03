515c0f85f9ea334e81b5bfce46504c0d23d15c80
coder <coder@int.world>
2023-01-03 16:03:29 -0500

    add configurations

---
+++ src/app/calculator/calculator.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculatorViewComponent } from './calculator-view/calculator-view.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    CalculatorViewComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ]
+})
+export class CalculatorModule { }
