b56d216eb752fe47e58e5c44f7bb8fd37c820151
AntonyJr00 <antonyrodl20@gmail.com>
2023-01-03 18:36:14 -0500

    maquetado hecho

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { CalculatorViewComponent } from './calculator-view/calculator-view.component';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
-  declarations: [
-    CalculatorViewComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+  declarations: [CalculatorViewComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [CalculatorViewComponent],
 })
-export class CalculatorModule { }
+export class CalculatorModule {}
