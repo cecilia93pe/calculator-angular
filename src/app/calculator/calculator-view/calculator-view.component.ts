6313056e2a6ae817dbbcba7070e8f5d4c7eb0881
AntonyJr00 <antonyrodl20@gmail.com>
2023-01-04 12:41:09 -0500

    operaciones hechas

--- src/app/calculator/calculator-view/calculator-view.component.ts
+++ src/app/calculator/calculator-view/calculator-view.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-calculator-view',
   templateUrl: './calculator-view.component.html',
-  styleUrls: ['./calculator-view.component.css']
+  styleUrls: ['./calculator-view.component.css'],
 })
 export class CalculatorViewComponent {
+  numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  private numeroActual: number = 0;
+  private numeroAnterior: number = 0;
+  private operador: string = '';
+  private resultado: number = 0;
+  private operaciones: string[] = ['+', '-', '*', '/'];
+  salida: string = '';
 
+  //--
+  agregar(valor: string): void {
+    this.salida = '';
+    if (this.operaciones.includes(valor)) {
+      this.operador = valor;
+      this.numeroActual = Number(this.salida.substring(2));
+      this.salida = this.salida + valor;
+    } else if (valor === '=') {
+      console.log(this.salida.split(this.operador)[1].substring(2));
+      this.numeroAnterior = Number(
+        this.salida.split(this.operador)[1].substring(2)
+      );
+      this.calcular();
+    } else if (valor === 'C') {
+      // this.limpiar();
+    } else if (this.salida === '') {
+      this.salida = 'S/' + valor;
+    } else if (this.salida[this.salida.length - 1] === this.operador) {
+      this.salida = this.salida + 'S/' + valor;
+    } else {
+      this.salida = this.salida + valor;
+    }
+  }
+
+  //---
+  calcular(): void {
+    switch (this.operador) {
+      case '+':
+        this.resultado = this.numeroActual + this.numeroAnterior;
+        break;
+      case '-':
+        this.resultado = this.numeroActual - this.numeroAnterior;
+        break;
+      case '*':
+        this.resultado = this.numeroActual * this.numeroAnterior;
+        break;
+      case '/':
+        this.resultado = this.numeroActual / this.numeroAnterior;
+        break;
+    }
+    this.salida = 'S/' + this.resultado.toString();
+    console.log(
+      this.numeroActual,
+      this.numeroAnterior,
+      this.operador,
+      this.resultado
+    );
+  }
+
+  //---
+  limpiar(): void {
+    this.salida = '0';
+  }
 }
