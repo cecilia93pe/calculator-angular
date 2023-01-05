e6209af2241e376909f3c59912197e94e196adf4
coder <coder@int.world>
2023-01-05 09:09:21 -0500

    add validations

--- src/app/calculator/calculator-view/calculator-view.component.ts
+++ src/app/calculator/calculator-view/calculator-view.component.ts
@@ -23,6 +23,13 @@ export class CalculatorViewComponent {
   date: any = this.today.toLocaleDateString("en-US", this.options)
   numbers: Array<string> = ['1','2','3','4','5','6','7','8','9','0']
   operators: Array<string> = ['÷', '×', '−', '+']
+  operatorsEval: any = {
+    '÷': '/',
+    '×': '*',
+    '−': '-',
+    '+': '+'
+  }
+
   category: CategoryType = {
     name: 'Groceries',
     color: '#3efa30',
@@ -30,7 +37,7 @@ export class CalculatorViewComponent {
   }
 
   previousNumber: string = ''
-  currentNumber: string = ''
+  currentNumber: string | null = null
   result: string = ''
   formatResult: string = ''
 
@@ -38,6 +45,7 @@ export class CalculatorViewComponent {
     $event.stopPropagation()
     const element = $event.currentTarget
     const action = element.dataset.calculator
+
     if (this.numbers.includes(action)) {
       if (this.currentNumber !== '0') {
         this.currentNumber += action
@@ -45,52 +53,44 @@ export class CalculatorViewComponent {
         this.formatResult = this.formatResult + action
       }
     }
-    console.log(this.currentNumber, 'currentnumebr')
-    if (action === 'decimal' && (this.currentNumber.indexOf('.') < 0)) {
-      if (this.currentNumber === '') {
-        this.currentNumber += '0.'
-        this.result = this.result + '0.'
-        this.formatResult = this.formatResult + '0.'
-      } else {
-        this.currentNumber += '.'
-        this.result = this.result + '.'
-        this.formatResult = this.formatResult + '.'
+    if ( action === 'decimal' ) {
+        if (this.currentNumber && this.currentNumber.indexOf('.') < 0) {
+
+          this.currentNumber += '.'
+          this.result = this.result + '.'
+          this.formatResult = this.formatResult + '.'
+        }
+
+      if (this.currentNumber === null) {
+        if (this.formatResult.length === 0) {
+          this.currentNumber = '0.'
+          this.result = this.result + '0.'
+          this.formatResult = this.formatResult + '0.'
+        }
       }
     }
     if (this.operators.includes(action) && this.currentNumber) {
-
-      switch (action) {
-        case this.operators[0]:
-          this.result += ' / '
-        break
-        case this.operators[1]:
-          this.result += ' * '
-        break
-        case this.operators[2]:
-          this.result += ' - '
-        break
-        case this.operators[3]:
-          this.result += ' + '
-          break
-      }
-      this.formatResult = this.formatResult + ' ' + action + ' '
-      this.currentNumber = ''
+      this.buildedResult(action)
+      this.currentNumber = null
     }
 
     if (action === 'calculate') {
-      this.formatResult = String(eval(this.result))
+      const lastElement = this.formatResult.slice(-1)
+
+      if (lastElement !== ' ') {
+
+        this.formatResult = String(eval(this.result))
+      }
     }
 
     if (action === 'clearAll') {
       this.formatResult = ''
       this.result = ''
-      this.currentNumber = ''
+      this.currentNumber = null
     }
 
     if (action === 'clear') {
-      console.log(this.formatResult, 'formatresul')
       const lastElement = this.formatResult.slice(-1)
-      console.log(lastElement, 'lastEleme')
       if (lastElement === ' ') {
         this.formatResult = this.formatResult.slice(0, -3)
         console.log('1')
@@ -98,6 +98,13 @@ export class CalculatorViewComponent {
         this.formatResult = this.formatResult.slice(0, -1)
         console.log('2')
       }
+    }
+  }
+
+  buildedResult = (operator: string) => {
+    if (!this.formatResult.split(' ').includes(operator)) {
+      this.result += ` ${this.operatorsEval[operator]} `
+      this.formatResult = this.formatResult + ' ' + operator + ' '
     }
   }
 
